2628229f0ce061a357dd96fb2decbf2d2346374e
Beu-Wolf <daniel.gseara@gmail.com>
2019-09-19 15:21:08 +0100

    headers included

---
+++ FS.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+#define max(A,B) ((A)>= (B) ? (A):(B))
+
+
+int main(void) {
+    
+}
---
+++ user.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) {
+    
+}
