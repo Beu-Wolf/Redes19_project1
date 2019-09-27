c2b572543cb240cc706cf67108c8789bb172f259
Marcelo Santos <marcelocmsantos@gmail.com>
2019-09-27 20:29:17 +0100

    Resolved #13 #15
    Server no longer sends "LTR "

--- server/topiclist.c
+++ server/topiclist.c
@@ -1,5 +1,7 @@
 #include "commands.h"
 
+#define DIRECTORY "./topics"
+
 char* processTopicList(char** tokenizedMessage) {
     char* topicListStatus = (char *)malloc(BUFFER_SIZE * sizeof(char));
     if (!topicListStatus) exit(1);
@@ -16,7 +18,7 @@ char* processTopicList(char** tokenizedMessage) {
 
     memset(topicsInfo, 0, BUFFER_SIZE);
 
-    DIR* dirp = opendir("./topics");
+    DIR* dirp = opendir(DIRECTORY);
     struct dirent* dp;
     int dircount = 0, n;
     int errno;
@@ -29,33 +31,37 @@ char* processTopicList(char** tokenizedMessage) {
 
     strncpy(topicListStatus, "LTR ", BUFFER_SIZE);
 
-    while(dirp) {
-        if((dp = readdir(dirp)) != NULL) {
-            if(strcmp(dp->d_name, ".") && strcmp(dp->d_name, "..") && dp->d_type == DT_DIR) {
-                dircount++;
-                sprintf(topicDatafile, "./topics/%s/%sdata", dp->d_name, dp->d_name);
-
-                FILE * topicData = fopen(topicDatafile, "r");
-                if (topicData == NULL) {
-                    printf("%d\n", errno);
-                    exit(1);
-                }
-
-                n = fread(topicUserID,  1, 5, topicData);
-                if(n == 0) exit(1);
-
-                sprintf(topicNameAndUser, " %s:%s", dp->d_name, topicUserID);
-                strcat(topicsInfo, topicNameAndUser);
-            }
-        } else {
-            sprintf(numDirs, "%d", dircount);
-            strcat(topicListStatus, numDirs);
-            strcat(topicListStatus, topicsInfo);
-            strcat(topicListStatus, "\n");
-
-            closedir(dirp);
-            return topicListStatus;
-        }
+    if (dirp == NULL) {
+	    strcat(topicListStatus, "0\n");
+	    mkdir(DIRECTORY, 0755);
+	    return topicListStatus;
     }
+
+    while (dp = readdir(dirp)) {
+	    if(strcmp(dp->d_name, ".") && strcmp(dp->d_name, "..") && dp->d_type == DT_DIR) {
+		    dircount++;
+		    sprintf(topicDatafile, DIRECTORY"/%s/%sdata", dp->d_name, dp->d_name);
+
+		    FILE * topicData = fopen(topicDatafile, "r");
+		    if (topicData == NULL) {
+			    printf("%d\n", errno);
+			    exit(1);
+		    }
+
+		    n = fread(topicUserID,  1, 5, topicData);
+		    if(n == 0) exit(1);
+
+		    sprintf(topicNameAndUser, " %s:%s", dp->d_name, topicUserID);
+		    strcat(topicsInfo, topicNameAndUser);
+	    }
+    }
+
+    sprintf(numDirs, "%d", dircount);
+    strcat(topicListStatus, numDirs);
+    strcat(topicListStatus, topicsInfo);
+    strcat(topicListStatus, "\n");
+
+    closedir(dirp);
+    return topicListStatus;
 
 }
