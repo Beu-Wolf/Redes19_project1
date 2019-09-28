755e4cb16beb37011924a9bd4638748ea148b67d
afonsocrg <afonso.crg@gmail.com>
2019-09-28 18:54:32 +0100

    defined error msgs

--- lib/util.h
+++ lib/util.h
@@ -1,6 +1,12 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+// Error messages
+#define NOT_REGISTERED_ERROR "You must be registered before performing any action\n"
+#define INVALID_RG_ARGS "Invalid arguments.\nUsage: register/reg <userID>\n"
+#define INVALID_TL_ARGS "Invalid arguments.\nUsage: topic_list/tl\n"
+#define INVALID_TS_ARGS "Invalid arguments.\nUsage: topic_select <topic>/ts <topic_number>\n"
+
 void fatal(char* buffer);
 void readCommand(char** bufPtr, int* bufSize);
 char **tokenize(char *string);
