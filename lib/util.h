c823f920f27376cdda17179cdb942bc916dd6f14
Beu-Wolf <daniel.gseara@gmail.com>
2019-10-01 16:47:20 +0100

    sendTCPstring function signature updated

--- lib/util.h
+++ lib/util.h
@@ -6,6 +6,9 @@
 #define INVALID_RG_ARGS "Invalid arguments.\nUsage: register/reg <userID>\n"
 #define INVALID_TL_ARGS "Invalid arguments.\nUsage: topic_list/tl\n"
 #define INVALID_TS_ARGS "Invalid arguments.\nUsage: topic_select <topic>/ts <topic_number>\n"
+#define INVALID_QS_ARGS "Invalid arguments.\nUsage: question_submit <question> <text_file> [<image_file.ext]\n"
+#define INVALID_QS_IMGEXT "Invalid image extension. Please select file with a permitted file extension\n"
+
 
 void fatal(char* buffer);
 void readCommand(char** bufPtr, int* bufSize);
@@ -14,8 +17,8 @@ int arglen(char **args);
 
 char isPositiveNumber(char* str);
 
-int sendString(int sockfd, char* buffer);
-int recv_line(int sockfd, char** buffer, int* size);
+int sendTCPstring(int sockfd, char* buffer);
+int recvTCPline(int sockfd, char** buffer, int* size);
 
 void stripnewLine(char* str);
 
