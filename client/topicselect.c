ba897f32f252f31173d1b87fe795ae009b218454
afonsocrg <afonso.crg@gmail.com>
2019-10-01 16:16:25 +0100

    check topicselect

--- client/topicselect.c
+++ client/topicselect.c
@@ -2,48 +2,35 @@
 
 void processTopicSelect(char** parsedInput, char** topicList){
 
-    int wantedNumber;
-    errno = 0;
+    int wantedNumber = -1;
+    char abbrev;
 
     if(userID == 0) {
       fprintf(stderr, NOT_REGISTERED_ERROR);
       return;
     }
-    if(arglen(parsedInput) > 2) {
+    if(arglen(parsedInput) != 2) {
       fprintf(stderr, INVALID_TS_ARGS);
       return;
     }
 
-    stripnewLine(parsedInput[1]);
-
-    if(strlen(parsedInput[0]) == 2) {
-        printf("Want topic number\n");
-        wantedNumber = strtol(parsedInput[1], NULL, 0);
-
-        if(errno != 0) {
+    abbrev = strlen(parsedInput[0]) == 2;
+    if(abbrev) {
+        errno = 0;
+        wantedNumber = strtol(parsedInput[1], NULL, 10);
+        if(errno != 0 || !isPositiveNumber(parsedInput[1])) {
             printf("Invalid number\n");
+            return;
         }
-
-        for(int i = 0 ; topicList[i] != 0; i++) {
-            if(i == wantedNumber){
-                selectedTopic = topicList[i];
-                printf("Topic selected: %s\n", selectedTopic);
-                return;
-            }
-        }
-
-        printf("Please select a number from the available topics\n");
-
-    } else {
-        printf("Want topic\n");
-
-        for(int i = 0; topicList[i] != 0; i++){
-            if(!strcmp(parsedInput[1], topicList[i])) {
-                selectedTopic = topicList[i];
-                printf("Topic selected: %s\n", selectedTopic);
-                return;
-            }
-        }
-        printf("Please select a valid topic name\n");
     }
+
+    stripnewLine(parsedInput[1]);
+    for(int i = 0; topicList[i] != 0; i++) {
+      if((abbrev && i == wantedNumber) || (!abbrev && !strcmp(parsedInput[1], topicList[i]))) {
+        selectedTopic = topicList[i];
+        printf("Selected topic: %s\n", selectedTopic);
+        return;
+      }
+    }
+    printf("Please select a valid topic\n");
 }
